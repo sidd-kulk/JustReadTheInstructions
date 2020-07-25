19422c39c201c30271262dfff74e8f2750c1beb8
SidK <sidcool1234@gmail.com>
2020-07-25 21:12:58 +0530

    Adds implementation of three flavors of code with run, runBlocking and launch functions

--- src/main/kotlin/com/sid/venkat/coroutines/Coroutines.kt
+++ src/main/kotlin/com/sid/venkat/coroutines/Coroutines.kt
@@ -1,5 +1,17 @@
 package com.sid.venkat.coroutines
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    nonCoroutine()
+    withCoroutine()
+    withCoroutineLaunch()
+
+    println("Master finish")
+}
+
+
 fun intTask(){
     println("intTask Starts from ${Thread.currentThread()}")
     for(i in 1 .. 222) {
@@ -16,16 +28,35 @@ fun stringTask(){
     println("stringTask ends from ${Thread.currentThread()}")
 }
 
-fun main() {
-    nonCoroutine()
-}
-
 private fun nonCoroutine() {
-    println("Start")
+    println("Start nonCoroutine -----------------")
     run {
         intTask()
         stringTask()
         println("Done with run block from ${Thread.currentThread()}")
     }
-    println("Stop")
+    println("Stop nonCoroutine -------------\n\n\n")
+}
+
+private fun withCoroutine() {
+    println("Start withCoroutine -------------")
+    runBlocking {
+        intTask()
+        stringTask()
+        println("Done with runBlocking block from ${Thread.currentThread()}")
+    }
+    println("Stop withCoroutine -------------\n" +
+            "\n" +
+            "\n")
+}
+
+
+private fun withCoroutineLaunch() {
+    println("Start withCoroutineLaunch -------------")
+    runBlocking {
+        launch {intTask() }
+        launch {stringTask()}
+        println("Done with runBlocking block from ${Thread.currentThread()}") // printed first
+    }
+    println("Stop withCoroutineLaunch -------------")
 }
