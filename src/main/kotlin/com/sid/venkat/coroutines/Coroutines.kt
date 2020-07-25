fe2d1ce23e683a6a9d6439a960e4821be2dbddce
SidK <sidcool1234@gmail.com>
2020-07-25 21:20:46 +0530

    Adds implementation of yield

--- src/main/kotlin/com/sid/venkat/coroutines/Coroutines.kt
+++ src/main/kotlin/com/sid/venkat/coroutines/Coroutines.kt
@@ -2,11 +2,13 @@ package com.sid.venkat.coroutines
 
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.yield
 
 fun main() {
     nonCoroutine()
     withCoroutine()
     withCoroutineLaunch()
+    withCoroutineAndYield()
 
     println("Master finish")
 }
@@ -14,6 +16,15 @@ fun main() {
 
 fun intTask(){
     println("intTask Starts from ${Thread.currentThread()}")
+    for(i in 1 .. 222) {
+        val j = i * 44
+    }
+    println("intTask ends from ${Thread.currentThread()}")
+}
+
+suspend fun intTaskWithYield(){
+    println("intTask Starts from ${Thread.currentThread()}")
+    yield()
     for(i in 1 .. 222) {
         val j = i * 44
     }
@@ -41,7 +52,7 @@ private fun nonCoroutine() {
 private fun withCoroutine() {
     println("Start withCoroutine -------------")
     runBlocking {
-        intTask()
+        intTaskWithYield()
         stringTask()
         println("Done with runBlocking block from ${Thread.currentThread()}")
     }
@@ -58,5 +69,23 @@ private fun withCoroutineLaunch() {
         launch {stringTask()}
         println("Done with runBlocking block from ${Thread.currentThread()}") // printed first
     }
-    println("Stop withCoroutineLaunch -------------")
+    println("Stop withCoroutineLaunch -------------\n\n\n\n")
 }
+
+private fun withCoroutineAndYield() {
+    println("withCoroutineAndYield")
+    runBlocking {
+        launch { intTaskWithYield() }
+        launch { stringTaskWithYield() }
+    }
+}
+
+suspend fun stringTaskWithYield() {
+    println("stringTask Starts from ${Thread.currentThread()}")
+    yield()
+    for(i in 1 .. 222) {
+        val j = "$i abc"
+    }
+    println("stringTask ends from ${Thread.currentThread()}")
+}
+
